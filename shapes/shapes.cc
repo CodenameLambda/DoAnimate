809d39ee4f46a1714cd5817f7f793d0b3135eb45
CNLambda <doris-books@dorimara.de>
2017-02-18 23:48:33 +0100

    Fixed compilation errors

--- shapes/shapes.cc
+++ shapes/shapes.cc
@@ -3,13 +3,6 @@
 
 namespace doanimate {
 	namespace shapes {
-		nptr line() {
-			return new nodes::default_node(
-					[](std::vector<types::value>) -> std::vector<types::value> {
-						return {};
-					},
-					{types::type_info
-			);
-		}
+		// TODO
 	}
 }
