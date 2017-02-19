eb30ae946fa435d30a5c10d0fdbc086ac4de8bf6
CNLambda <doris-books@dorimara.de>
2017-02-19 11:59:08 +0100

    Changed type names to lowercase in node.cc

--- nodes/node.cc
+++ nodes/node.cc
@@ -3,27 +3,27 @@
 
 namespace doanimate {
 	namespace nodes {
-		void default_node::update() {
+		void DefaultNode::update() {
 			outputs = implementation(inputs);
 		}
 
 
-		std::vector<types::value>& default_node::get_inputs() {
+		std::vector<types::Value>& DefaultNode::get_inputs() {
 			return inputs;
 		}
 
 		 
-		const std::vector<types::value>& default_node::get_outputs() {
+		const std::vector<types::Value>& DefaultNode::get_outputs() {
 			return outputs;
 		}
 
 
-		const std::vector<types::type_info>& default_node::get_input_type_infos() {
+		const std::vector<types::TypeInfo>& DefaultNode::get_input_type_infos() {
 			return input_type_infos;
 		}
 
 
-		const std::vector<types::type_info>& default_node::get_output_type_infos() {
+		const std::vector<types::TypeInfo>& DefaultNode::get_output_type_infos() {
 			return output_type_infos;
 		}
 	}
